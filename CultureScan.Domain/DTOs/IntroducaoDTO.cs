79f1caf81f509c607f1460340bc9fcc7a3fdece8
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-01 18:58:19 -0300

    feat: criacao introducaoDTO

---
+++ CultureScan.Domain/DTOs/IntroducaoDTO.cs
@@ -0,0 +1,11 @@
+﻿using CultureScan.Domain.Enums;
+
+namespace CultureScan.Domain.DTOs
+{
+    public class IntroducaoDTO
+    {
+        public string Setor { get; set; }
+        public TempoDeTrabalhoEnum TempoDeTrabalhoEnum { get; set; }
+        public string Regiao { get; set; }
+    }
+}
