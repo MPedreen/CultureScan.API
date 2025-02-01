4e555f4af99c0c7e5bfff6c228be63bd66d1e832
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-01 18:58:12 -0300

    feat: criacao tempo de trabalho enum

---
+++ CultureScan.Domain/Enums/TempoDeTrabalhoEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace CultureScan.Domain.Enums
+{
+    public enum TempoDeTrabalhoEnum
+    {
+        De0A2Anos = 1,
+        De2A5Anos = 2,
+        De5A10Anos = 3,
+        MaisDe10Anos = 4
+    }
+}
