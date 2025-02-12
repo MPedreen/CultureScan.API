613c6a9f734c9718993280a9abd81e3d075e2881
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-11 22:22:00 -0300

    feat: criando feedback controller

---
+++ CultureScan.API/Controllers/FeedbackController.cs
@@ -0,0 +1,12 @@
+﻿using CultureScan.Domain.Perguntas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CultureScan.API.Controllers
+{
+    public class FeedbackController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PerguntaDto>> GetQuestions()
+            => Ok(Perguntas);
+    }
+}
