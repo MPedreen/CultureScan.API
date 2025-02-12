1914d9332d828d3b9ecb9bc47f63a1f4fb3452ff
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-12 20:43:26 -0300

    feat: criacao mock pergunta processos

---
+++ CultureScan.Domain/Perguntas/Processos.cs
@@ -0,0 +1,17 @@
+﻿using CultureScan.Domain.Base;
+
+namespace CultureScan.Domain.Perguntas
+{
+    public class Processos : Pergunta
+    {
+        public override string Categoria => "processos";
+
+        public override ICollection<PerguntaDto> ObterPerguntas() => new List<PerguntaDto>
+        {
+            new() { Pergunta = "Como você avalia o nível de serviço que a empresa oferece aos seus clientes em termos de qualidade e eficiência?" },
+            new() { Pergunta = "Na sua opinião, qual é o nível de satisfação dos clientes com os serviços prestados pela empresa?" },
+            new() { Pergunta = "Você sente que tem autonomia suficiente para oferecer um atendimento de excelência aos clientes, dentro das suas responsabilidades?" },
+            new() { Pergunta = "Você percebe que a INFIS tem um foco consistente e eficaz na busca pela satisfação do cliente em todas as suas ações e decisões?" }
+        };
+    }
+}
