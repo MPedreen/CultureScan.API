27ea9fbe1a7e42fb840e7d54b2fd0d34a8c31d44
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-11 22:22:09 -0300

    feat: criando pergunta model base

---
+++ CultureScan.Domain/Base/Pergunta.cs
@@ -0,0 +1,10 @@
+﻿using CultureScan.Domain.Perguntas;
+
+namespace CultureScan.Domain.Base
+{
+    public abstract class Pergunta : Entity<Pergunta>
+    {
+        public abstract string Categoria { get; }
+        public abstract ICollection<PerguntaDto> ObterPerguntas();
+    }
+}
