7a3d282b17cb717b8f8311566282e17466418e0c
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-11 22:22:20 -0300

    feat: criando model mock comunicacao

---
+++ CultureScan.Domain/Perguntas/Comunicacao.cs
@@ -0,0 +1,17 @@
+﻿using CultureScan.Domain.Base;
+
+namespace CultureScan.Domain.Perguntas
+{
+    public class Comunicacao : Pergunta
+    {
+        public override string Categoria => "comunicacao";
+
+        public override ICollection<PerguntaDto> ObterPerguntas() => new()
+        {
+            new PerguntaDto { Pergunta = "Você tem uma compreensão clara dos objetivos da sua área?" },
+            new PerguntaDto { Pergunta = "As suas responsabilidades no cargo estão bem definidas?" },
+            new PerguntaDto { Pergunta = "Você sente que tem liberdade para dar sugestões?" },
+            new PerguntaDto { Pergunta = "Como avalia a comunicação com sua chefia?" }
+        };
+    }
+}
