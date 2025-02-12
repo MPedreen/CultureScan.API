89db2e489ee3e4b465a29ad1bfe1808a3d1ecfcc
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-11 21:07:33 -0300

    feat: criacao entity base

---
+++ CultureScan.Domain/Base/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace CultureScan.Domain.Base
+{
+    public abstract class Entity<T> where T : Entity<T>
+    {
+        public Guid Id { get; protected set; }
+        public DateTime DataCriacao { get; protected set; }
+        public DateTime DataUltimaAtualizacao { get; protected set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+            DataCriacao = DateTime.UtcNow;
+            DataUltimaAtualizacao = DateTime.UtcNow;
+        }
+    }
+}
