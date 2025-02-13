51b2434efd13fe117e366c8dcfc6e2dfa79c17a4
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-13 19:35:50 -0300

    fix: corrigindo perguntas genericas remuneracao

--- CultureScan.Domain/Perguntas/Remuneracao.cs
+++ CultureScan.Domain/Perguntas/Remuneracao.cs
@@ -8,15 +8,9 @@ namespace CultureScan.Domain.Perguntas
 
         public override ICollection<PerguntaDto> ObterPerguntas() => new List<PerguntaDto>
         {
-            new() { Pergunta = "Você está satisfeito com a cobertura e qualidade do plano de assistência médica, que se estende aos seus dependentes (filhos)?" },
-            new() { Pergunta = "Você está satisfeito com a cobertura e qualidade do plano de assistência odontológica, que também abrange seus dependentes (filhos) - se aplicável?" },
-            new() { Pergunta = "Você considera o seguro de vida oferecido pela empresa adequado e satisfatório para suas necessidades e de seus dependentes?" },
-            new() { Pergunta = "Você considera que o benefício de alimentação (Flash Benefícios) e a flexibilidade oferecida atendem às suas necessidades e expectativas?" },
-            new() { Pergunta = "O benefício do Gympass, que oferece acesso a academias e atividades físicas, atende suas expectativas e contribui para o seu bem-estar?" },
-            new() { Pergunta = "Como você avalia a sua remuneração em comparação aos demais colaboradores da empresa que ocupam cargos similares?" },
-            new() { Pergunta = "Você considera que a sua remuneração está alinhada com os profissionais que atuam nas mesmas funções em outras empresas do setor?" },
-            new() { Pergunta = "Você está satisfeito com o sistema de bonificação por resultados oferecido pela empresa?" },
-            new() { Pergunta = "Você considera que o percentual de bonificação é definido e apresentado de maneira clara e satisfatória?" }
+            new() { Pergunta = "Você está satisfeito com os benefícios oferecidos pela empresa?" },
+            new() { Pergunta = "Você considera sua remuneração justa em comparação com colaboradores de cargos similares na empresa?" },
+            new() { Pergunta = "Você acredita que sua remuneração está alinhada com o mercado para sua função?" },
         };
     }
 }
