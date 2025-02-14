8892b49e699dca626316fea1754aee16b05ce975
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 20:59:59 -0300

    feat: criando interface repository remuneracao

---
+++ CultureScan.Infra/Perguntas/IRemuneracaoRepository.cs
@@ -0,0 +1,9 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Base;
+
+namespace CultureScan.Infra.Repositories.Abstractions.Perguntas;
+
+public interface IRemuneracaoRepository : IRepository<Remuneracao>
+{
+
+}
