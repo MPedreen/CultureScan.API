d556e45a3fd13670b3cd6c158e5c9da59cd7a0e5
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-13 21:18:08 -0300

    fix: renomeando feedback controller para pergunta controller

---
+++ CultureScan.API/Controllers/PerguntaController.cs
@@ -0,0 +1,26 @@
+﻿using CultureScan.Domain.Perguntas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CultureScan.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PerguntaController : ControllerBase
+{
+    [HttpGet("{pergunta}")]
+    public ActionResult<IEnumerable<PerguntaDto>> ObterPorCategoria(TipoCategoria categoria)
+    {
+        var perguntas = categoria switch
+        {
+            TipoCategoria.Carreira => new Carreira().ObterPerguntas(),
+            TipoCategoria.Chefia => new Chefia().ObterPerguntas(),
+            TipoCategoria.Comunicacao => new Comunicacao().ObterPerguntas(),
+            TipoCategoria.Corporativo => new Corporativo().ObterPerguntas(),
+            TipoCategoria.Processo => new Processo().ObterPerguntas(),
+            TipoCategoria.Remuneracao => new Remuneracao().ObterPerguntas(),
+            _ => null
+        };
+
+        return Ok(perguntas);
+    }
+}
