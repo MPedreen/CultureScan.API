56f8bffea536e4bf0837c86747e120e1ea732062
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 20:57:06 -0300

    fix: removendo switch case que nao sera usado na pergunta controller

--- CultureScan.API/Controllers/PerguntaController.cs
+++ CultureScan.API/Controllers/PerguntaController.cs
@@ -1,26 +1,9 @@
-﻿using CultureScan.Domain.Perguntas;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CultureScan.API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
+[Route("perguntas")]
 public class PerguntaController : ControllerBase
 {
-    [HttpGet("{pergunta}")]
-    public ActionResult<IEnumerable<PerguntaDto>> ObterPorCategoria(TipoCategoria categoria)
-    {
-        var perguntas = categoria switch
-        {
-            TipoCategoria.Carreira => new Carreira().ObterPerguntas(),
-            TipoCategoria.Chefia => new Chefia().ObterPerguntas(),
-            TipoCategoria.Comunicacao => new Comunicacao().ObterPerguntas(),
-            TipoCategoria.Corporativo => new Corporativo().ObterPerguntas(),
-            TipoCategoria.Processo => new Processo().ObterPerguntas(),
-            TipoCategoria.Remuneracao => new Remuneracao().ObterPerguntas(),
-            _ => null
-        };
 
-        return Ok(perguntas);
-    }
 }
