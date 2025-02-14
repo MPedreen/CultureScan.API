ecdc78b50918769694e952df516429af0dd91581
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-13 21:17:27 -0300

    feat: criando pergunta configuration e definindo props

---
+++ CultureScan.Infra.Repositories/Configurations/PerguntaConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using CultureScan.Domain.Perguntas;
+
+namespace CultureScan.Infra.Repositories.Configurations
+{
+    public class PerguntaConfiguration : IEntityTypeConfiguration<Pergunta>
+    {
+        public void Configure(EntityTypeBuilder<Pergunta> perguntas)
+        {
+            perguntas
+                .HasKey(key => key.Id);
+
+            perguntas
+                .Property(prop => prop.Descricao)
+                .IsRequired();
+        }
+    }
+}
