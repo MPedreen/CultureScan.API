467335b99b1f9182202b3a15851e2ab428fffc30
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 20:57:20 -0300

    fix: correcao namespace entity base

--- CultureScan.Domain/Base/Entity.cs
+++ CultureScan.Domain/Base/Entity.cs
@@ -1,16 +1,15 @@
-﻿namespace CultureScan.Domain.Base
-{
-    public abstract class Entity<T> where T : Entity<T>
-    {
-        public Guid Id { get; protected set; }
-        public DateTime DataCriacao { get; protected set; }
-        public DateTime DataUltimaAtualizacao { get; protected set; }
+﻿namespace CultureScan.Domain.Base;
 
-        protected Entity()
-        {
-            Id = Guid.NewGuid();
-            DataCriacao = DateTime.UtcNow;
-            DataUltimaAtualizacao = DateTime.UtcNow;
-        }
+public abstract class Entity<T> where T : Entity<T>
+{
+    public Guid Id { get; protected set; }
+    public DateTime DataCriacao { get; protected set; }
+    public DateTime DataUltimaAtualizacao { get; protected set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        DataCriacao = DateTime.UtcNow;
+        DataUltimaAtualizacao = DateTime.UtcNow;
     }
 }
