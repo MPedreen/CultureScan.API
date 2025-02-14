8937aa9d1c6fb75c4848fc81eceab837c982d9e9
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-13 21:19:01 -0300

    feat: movendo model pergunta para pasta perguntas

--- CultureScan.Domain/Perguntas/Pergunta.cs
+++ CultureScan.Domain/Perguntas/Pergunta.cs
@@ -1,10 +1,11 @@
-﻿using CultureScan.Domain.Perguntas;
+﻿using CultureScan.Domain.Base;
 
-namespace CultureScan.Domain.Base
+namespace CultureScan.Domain.Perguntas
 {
     public abstract class Pergunta : Entity<Pergunta>
     {
         public abstract string Categoria { get; }
+        public string Descricao { get; }
         public abstract ICollection<PerguntaDto> ObterPerguntas();
     }
 }
