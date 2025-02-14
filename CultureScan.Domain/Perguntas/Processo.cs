b8821e86ab4d68c91f79144e06a09a6832edc36e
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 20:58:28 -0300

    fix: desacoplando perguntas da model processo

--- CultureScan.Domain/Perguntas/Processo.cs
+++ CultureScan.Domain/Perguntas/Processo.cs
@@ -1,15 +1,8 @@
-﻿namespace CultureScan.Domain.Perguntas
-{
-    public class Processo : Pergunta
-    {
-        public override string Categoria => "processos";
+﻿using CultureScan.Domain.Base;
 
-        public override ICollection<PerguntaDto> ObterPerguntas() => new List<PerguntaDto>
-        {
-            new() { Pergunta = "Como você avalia o nível de serviço que a empresa oferece aos seus clientes em termos de qualidade e eficiência?" },
-            new() { Pergunta = "Na sua opinião, qual é o nível de satisfação dos clientes com os serviços prestados pela empresa?" },
-            new() { Pergunta = "Você sente que tem autonomia suficiente para oferecer um atendimento de excelência aos clientes, dentro das suas responsabilidades?" },
-            new() { Pergunta = "Você percebe que a INFIS tem um foco consistente e eficaz na busca pela satisfação do cliente em todas as suas ações e decisões?" }
-        };
-    }
+namespace CultureScan.Domain.Perguntas;
+
+public class Processo : Entity<Processo>
+{
+    public string Descricao { get; set; }
 }
