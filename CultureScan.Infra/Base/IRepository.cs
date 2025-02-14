5908dcb0cd0ee7395e70c35c543f6cb2396fa1be
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 20:58:54 -0300

    feat: criando interface generica irepository

---
+++ CultureScan.Infra/Base/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using CultureScan.Domain.Base;
+using System.Linq.Expressions;
+
+namespace CultureScan.Infra.Repositories.Abstractions.Base;
+
+public interface IRepository<TEntity> where TEntity : Entity<TEntity>
+{
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IEnumerable<TEntity> entities);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+    Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<IReadOnlyList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null);
+}
