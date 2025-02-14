c7a16253349214a79fdeed8156fcd694b606968b
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-13 21:18:38 -0300

    feat: criando tipo categoria enum

---
+++ CultureScan.Domain/Perguntas/TipoCategoria.cs
@@ -0,0 +1,12 @@
+﻿namespace CultureScan.Domain.Perguntas
+{
+    public enum TipoCategoria
+    {
+        Carreira = 1,
+        Chefia = 2,
+        Comunicacao = 3,
+        Corporativo = 4,
+        Processo = 5,
+        Remuneracao = 6
+    }
+}
