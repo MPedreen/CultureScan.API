5205fccff48df6a8d1165105fc69c88312074025
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-13 21:19:26 -0300

    feat: criando culture scan context e configurando para pegar configurations por aseembly

---
+++ CultureScan.Infra.Repositories/Context/CultureScanContext.cs
@@ -0,0 +1,14 @@
+﻿using CultureScan.Domain.Perguntas;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CultureScan.Infra.Repositories.Context;
+public class CultureScanContext : DbContext
+{
+    public DbSet<Pergunta> Perguntas { get; set; }
+
+    public CultureScanContext(DbContextOptions<CultureScanContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+        => builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+}
