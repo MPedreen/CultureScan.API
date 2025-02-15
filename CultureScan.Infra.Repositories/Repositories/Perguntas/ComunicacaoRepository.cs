26411c53f5a266170c986fd9ccbde5d95bbed1f7
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:02:35 -0300

    feat: criando repository comunicacao

---
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/ComunicacaoRepository.cs
@@ -0,0 +1,12 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Perguntas;
+using CultureScan.Infra.Repositories.Context;
+
+namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
+
+public class ComunicacaoRepository : AbstractRepository<Comunicacao>, IComunicacaoRepository
+{
+    public ComunicacaoRepository(CultureScanContext context) : base(context.Comunicacoes, context)
+    {
+    }
+}
