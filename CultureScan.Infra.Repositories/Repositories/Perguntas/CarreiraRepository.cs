5d18b574cc4674ee01a07dd2886318e1d0369743
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:02:22 -0300

    feat: criando repository carreira

---
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/CarreiraRepository.cs
@@ -0,0 +1,12 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Perguntas;
+using CultureScan.Infra.Repositories.Context;
+
+namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
+
+public class CarreiraRepository : AbstractRepository<Carreira>, ICarreiraRepository
+{
+    public CarreiraRepository(CultureScanContext context) : base(context.Carreiras, context)
+    {
+    }
+}
