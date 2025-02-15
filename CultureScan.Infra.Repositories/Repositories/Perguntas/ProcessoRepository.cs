705b4abceba8750ac683e79c0f842c5f5c8f6c05
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:02:52 -0300

    feat: criando repository processo

---
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/ProcessoRepository.cs
@@ -0,0 +1,12 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Perguntas;
+using CultureScan.Infra.Repositories.Context;
+
+namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
+
+public class ProcessoRepository : AbstractRepository<Processo>, IProcessoRepository
+{
+    public ProcessoRepository(CultureScanContext context) : base(context.Processos, context)
+    {
+    }
+}
