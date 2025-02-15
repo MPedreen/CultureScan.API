8d7d7beb4cb51b3910e6ea5f6c1070a8588176a6
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:01:40 -0300

    feat: criando db context

--- CultureScan.Infra.Repositories/Context/CultureScanContext.cs
+++ CultureScan.Infra.Repositories/Context/CultureScanContext.cs
@@ -3,9 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace CultureScan.Infra.Repositories.Context;
+
 public class CultureScanContext : DbContext
 {
-    public DbSet<Pergunta> Perguntas { get; set; }
+    public DbSet<Carreira> Carreiras { get; set; }
+    public DbSet<Chefia> Chefias { get; set; }
+    public DbSet<Comunicacao> Comunicacoes { get; set; }
+    public DbSet<Corporativo> Corporativos { get; set; }
+    public DbSet<Processo> Processos { get; set; }
+    public DbSet<Remuneracao> Remuneracoes { get; set; }
 
     public CultureScanContext(DbContextOptions<CultureScanContext> options) : base(options) { }
 
