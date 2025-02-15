cfb4fd7085efe34eb796544ddd26c17bd853eac9
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:02:27 -0300

    feat: criando repository chefia

---
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/ChefiaRepository.cs
@@ -0,0 +1,12 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Perguntas;
+using CultureScan.Infra.Repositories.Context;
+
+namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
+
+public class ChefiaRepository : AbstractRepository<Chefia>, IChefiaRepository
+{
+    public ChefiaRepository(CultureScanContext context) : base(context.Chefias, context)
+    {
+    }
+}
