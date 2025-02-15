f800cb97c54a074e5fc23859581904a6abe9ab4e
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:00:18 -0300

    feat: criando interface iunitofwork

---
+++ CultureScan.Infra/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CultureScan.Domain.Base;
+using CultureScan.Infra.Repositories.Abstractions.Base;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CultureScan.Infra.Repositories.Abstractions.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        Task CommitTransactionAsync(IDbContextTransaction transaction);
+        Task RollBackTransactionAsync(IDbContextTransaction transaction);
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : Entity<TEntity>;
+    }
+}
