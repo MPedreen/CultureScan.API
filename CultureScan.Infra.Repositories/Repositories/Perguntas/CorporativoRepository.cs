dd06fc1f8fd27068cbd7b491c6b5658c4a121b90
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:02:44 -0300

    feat: criando repository corporativo

---
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/CorporativoRepository.cs
@@ -0,0 +1,12 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Perguntas;
+using CultureScan.Infra.Repositories.Context;
+
+namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
+
+public class CorporativoRepository : AbstractRepository<Corporativo>, ICorporativoRepository
+{
+    public CorporativoRepository(CultureScanContext context) : base(context.Corporativos, context)
+    {
+    }
+}
