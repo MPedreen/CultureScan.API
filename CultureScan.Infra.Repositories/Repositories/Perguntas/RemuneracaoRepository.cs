fe852abab6c93537e02393c185a8ab48329a8ed7
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-14 21:02:59 -0300

    feat: criando repository remuneracao

---
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/RemuneracaoRepository.cs
@@ -0,0 +1,13 @@
+﻿using CultureScan.Domain.Perguntas;
+using CultureScan.Infra.Repositories.Abstractions.Perguntas;
+using CultureScan.Infra.Repositories.Context;
+using CultureScan.Infra.Repositories.Repositories.Base;
+
+namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
+
+public class RemuneracaoRepository : Repository<Remuneracao>, IRemuneracaoRepository
+{
+    public RemuneracaoRepository(CultureScanContext context) : base(context.Remuneracoes, context)
+    {
+    }
+}
