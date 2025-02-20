31244a1cd62e1af160ac46dbf0f9c03c72cde544
Pedro Ferreira <pedrophpvp12@gmail.com>
2025-02-20 20:39:20 -0300

    fix: corrigindo processo repository generico heranca

--- CultureScan.Infra.Repositories/Repositories/Perguntas/ProcessoRepository.cs
+++ CultureScan.Infra.Repositories/Repositories/Perguntas/ProcessoRepository.cs
@@ -1,10 +1,11 @@
 ﻿using CultureScan.Domain.Perguntas;
 using CultureScan.Infra.Repositories.Abstractions.Perguntas;
 using CultureScan.Infra.Repositories.Context;
+using CultureScan.Infra.Repositories.Repositories.Base;
 
 namespace CultureScan.Infra.Repositories.Repositories.Perguntas;
 
-public class ProcessoRepository : AbstractRepository<Processo>, IProcessoRepository
+public class ProcessoRepository : Repository<Processo>, IProcessoRepository
 {
     public ProcessoRepository(CultureScanContext context) : base(context.Processos, context)
     {
